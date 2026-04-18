ec69cd1b03d2e0fde96b620fbbbdecdb8e631cb5
Rakshitha R <rakshitharr2196540@gmail.com>
2026-04-18 21:55:21 +0530

    Update setup.sh

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "Starting Minikube..."
+minikube start --driver=docker
+
+echo "Enabling metrics..."
+minikube addons enable metrics-server
+
+echo "Deploying app..."
+kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml
+
+echo "Checking pods..."
+kubectl get pods
